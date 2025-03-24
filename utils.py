6e09068659da5a9ceceb4ae305cb8a2d6bd30f4d
LuisSoaresBM <ldiass@live.com>
2025-03-24 18:12:39 +0100

    Improve utils

--- utils.py
+++ utils.py
@@ -5,14 +5,16 @@ def business_days_calc(num_days: int, start_dow: int) -> int:
     https://stackoverflow.com/questions/43541944/fastest-algorithm-to-calculate-the-number-of-business-days-between-two-dates 
 
     :param num_days: Total days difference between start and end date.
-    :param start_dow: Day of the week of the start date (0 = Monday, 6 = Sunday)
+    :param start_dow: Day of the week of the start date (0 = Sunday, 6 = Saturday)
     :return: Number of business days.
     """
     num_holidays = (num_days // 7) * 2  
     
     remaining_days = num_days % 7  
 
-    if start_dow == 1:  # Monday
+    if start_dow == 0:  # Sunday
+        num_holidays += + (1 if remaining_days > 5 else 0) 
+    elif start_dow == 1:  # Monday
         num_holidays += min(remaining_days - 4, 2) if remaining_days > 4 else 0
     elif start_dow == 2:  # Tuesday
         num_holidays += min(remaining_days - 3, 2) if remaining_days > 3 else 0
@@ -24,7 +26,5 @@ def business_days_calc(num_days: int, start_dow: int) -> int:
         num_holidays += min(remaining_days, 2) if remaining_days > 0 else 0
     elif start_dow == 6:  # Saturday
         num_holidays += + (1 if remaining_days > 0 else 0)
-    elif start_dow == 0:  # Sunday
-        num_holidays += + (1 if remaining_days > 5 else 0)
 
     return num_days - num_holidays
